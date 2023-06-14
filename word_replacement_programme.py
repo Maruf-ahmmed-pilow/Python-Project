adc5147f77ab6089b1f5e1b73a42c3ae3e042f13
Maruf ahmmed pilow <81496426+Maruf-ahmmed-pilow@users.noreply.github.com>
2023-06-14 17:04:52 +0600

    Add files via upload

---
+++ word_replacement_programme.py
@@ -0,0 +1,8 @@
+def replace_word():
+    str = "hlw this is pilow , and hi hi hi"
+    word_to_replace=input("Enter the word to replace: ")
+    word_to_replacement = input("Enter the word replacement: ")
+    
+    print(str.replace(word_to_replace, word_to_replacement))
+    
+replace_word()
