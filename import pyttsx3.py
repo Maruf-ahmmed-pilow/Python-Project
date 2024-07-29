9e037e87f2c533206495ad6d382f5f390d4e78f4
Maruf ahmmed pilow <81496426+Maruf-ahmmed-pilow@users.noreply.github.com>
2024-07-29 11:38:05 +0600

    Add files via upload

---
+++ import pyttsx3.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hi peelow welcome , i'm your robot, from artificial inteligency, ask me anything. what you want to know from me")
+engine.runAndWait()
